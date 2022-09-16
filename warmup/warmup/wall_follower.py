63bca861a5aeae4e598f2188773036f9c3724584
Melody Chiu <cchiu@olin.edu>
2022-09-16 14:43:02 -0400

    complete wall follower

--- warmup/warmup/wall_follower.py
+++ warmup/warmup/wall_follower.py
@@ -3,53 +3,36 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from visualization_msgs.msg import Marker, MarkerArray
+from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 
-# from teleop import TeleopNode
-
 class WallFollowerNode(Node):
-    dist = 0
+    front_dist = 0
+    back_dist = 0
 
     def __init__(self):
         super().__init__('receive_message_node')
         self.sub = self.create_subscription(LaserScan, 'scan', self.process_laserscan, 10)
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.timer = self.create_timer(1, self.run_loop)
         self.vis_pub = self.create_publisher(Marker, 'visualization_marker', 10)
+        self.timer = self.create_timer(1, self.run_loop)
 
     def process_laserscan(self, msg):
-        # print("0: ", msg.ranges[0])
-        # print("10: ", msg.ranges[10])
-        # print("20: ", msg.ranges[20])
-        # print("30: ", msg.ranges[30])
-        # print("45: ", msg.ranges[45])
-        self.dist = msg.ranges[55]
-        print("55: ", self.dist)
-        # print("70: ", msg.ranges[70])
-        # print("85: ", msg.ranges[85])
-        # print("120: ", msg.ranges[120])
-        # print("140: ", msg.ranges[140])
-        # print("180: ", msg.ranges[180])
-        # print("270: ", msg.ranges[270])
+        self.front_dist = msg.ranges[60]
+        self.back_dist = msg.ranges[120]
+        print("front: ", self.front_dist, "back: ", self.back_dist)
 
     def run_loop(self):
-        # needs work
         my_twist_velocity = Twist()
         my_twist_velocity.linear.x = 0.2
-        if self.dist > 0.8:
+        dist = self.front_dist - self.back_dist
+        if dist > 0.1:
             print("adjusting to left")
             my_twist_velocity.angular.z = 0.1
             self.pub.publish(my_twist_velocity)
-            sleep(0.5)
-            my_twist_velocity.angular.z = 0.0
-            self.pub.publish(my_twist_velocity)
-        if self.dist < 0.6:
+        if dist < -0.1:
             print("adjusting to right")
             my_twist_velocity.angular.z = -0.1
-            self.pub.publish(my_twist_velocity)
-            sleep(0.5)
-            my_twist_velocity.angular.z = 0.0
             self.pub.publish(my_twist_velocity)
         self.pub.publish(my_twist_velocity)
         self.publish_marker()
@@ -75,11 +58,11 @@ class WallFollowerNode(Node):
         marker.points.append(Point(x=5.0, y=4.0, z=0.0))
         marker.points.append(Point(x=8.5, y=4.8, z=0.0))
         marker.points.append(Point(x=14.0, y=4.0, z=0.0))
+        self.vis_pub.publish(marker)
 
 def main(args=None):
     rclpy.init(args=args)         # Initialize communication with ROS
     node = WallFollowerNode()     # Create our Node
-    # teleopNode = TeleopNode()           # Create our Node
     rclpy.spin(node)              # Run the Node until ready to shutdown
     rclpy.shutdown()              # cleanup
 
